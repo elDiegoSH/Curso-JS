3a3e5ad31d2f99cdf639b1af92d4b7ef0a554c56
DiegoSH <hola@diegosh.com>
2021-03-10 11:12:15 -0600

    Primer commit, agregando archivos

---
+++ 01-Fundamentos/app.js
@@ -0,0 +1,18 @@
+
+// console.log('Hola Mundo desde Js');
+
+let a = 10,
+    b = 20,
+    c = 'Hola ',
+    d = 'Spiderman',
+    x = a + b;
+
+    // console.log('%c Mis variables', 'color:green; font-weight: bold');
+    // console.log( {a} );
+    // console.log({b} );
+    // console.log( {c} );
+
+    // console.table({ a, b, c, d, x});
+
+     const saludo = c + d;
+    // console.log(saludo);
