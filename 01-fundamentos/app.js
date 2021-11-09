a1d2203ce9b763d4c9e2f9726a04be60b99a78f6
DiegoSH <hola@diegosh.com>
2021-11-09 16:59:24 -0600

    Se agregan y suben archivos

---
+++ 01-fundamentos/app.js
@@ -0,0 +1,18 @@
+
+// console.log('Hola Mundo');
+
+let a = 10,
+    b = 20,
+    c = 'Hola ',
+    d = 'Spiderman',
+    x = a + b;
+
+const saludo = c + d;
+    
+
+    // console.log ( { a } );
+    // console.log ( { b } );
+    // console.log ( { c } );
+
+    // console.table({a,b,c,d,x});
+    console.log(saludo);
