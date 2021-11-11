bd1a62bd96c71f2b0d92c25a43b96a756c20899b
DIEGOSH ROJA <diegosh84@gmail.com>
2021-11-11 14:43:55 -0600

    Practica de prompt, confirm y alert

---
+++ 01-fundamentos/assets/js/alerts.js
@@ -0,0 +1,16 @@
+// *** Alert ***
+// alert('Mensaje de prueba');
+
+// let nombre = prompt('¿Cual es tu nombre?');
+
+// console.log(nombre);
+
+// console.log('****' + nombre + '****');
+
+
+
+// *** Confirm ***
+// confirm('¿Quieres borra esto?');
+
+const seleccion = confirm('¿Quieres borra esto?');
+console.log(seleccion);
