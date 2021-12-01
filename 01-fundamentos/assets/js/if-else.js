b571df16f4b9abc2a3d882b6e3cbf1331eab1a4f
Diegosh <dsanchez@diegosh.com>
2021-12-01 12:21:21 -0600

    Laboratorio de if y else

--- 01-fundamentos/assets/js/if-else.js
+++ 01-fundamentos/assets/js/if-else.js
@@ -24,3 +24,25 @@ if ( dia === 0){
 }else {
     console.log('No es Lunes, Martes o Domingo');
 }
+
+
+// SIN USAR IF, ELSE O SWITCH UNICAMENTE OBJETOS
+dia = hoy.getDay();
+
+// METODO 1, COMO OBJETO
+const diaLetras = {
+    0: 'domingo',
+    1: 'lunes',
+    2: 'martes',
+    3: 'miercoles',
+    4: 'jueves',
+    5: 'viernes',
+    6: 'sabado',
+}
+
+// METODO 2, COMO ARREGLO
+const diaLetras2 = ['domingo', 'lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado',];
+
+// dia de la semana
+console.log(diaLetras[dia] || 'Día no válido');
+console.log(diaLetras2[dia] || 'Día no válido' );
